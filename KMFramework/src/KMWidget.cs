607dfa2238596426c9c7fd7b4c3c362972d97519
bfdev <brian.fetter.dev@gmail.com>
2016-06-29 18:54:28 -0400

    Added widget support to mod framework.

---
+++ KMFramework/src/KMWidget.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class KMWidget : MonoBehaviour
+{
+    public int SizeX;
+    public int SizeZ;
+
+    public delegate void KMWidgetActivateDelegate();
+    public KMWidgetActivateDelegate OnWidgetActivate;
+
+    public delegate string KMWidgetQueryDelegate(string queryKey, string queryInfo);
+    public KMWidgetQueryDelegate OnQueryRequest;
+
+    public static Vector3 BaseSize = new Vector3(0.06f, 0.03f, 0.06f);
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = new Color(1f, 0f, 0f, 0.3f);
+        Vector3 aSize = new Vector3(BaseSize.x * SizeX, BaseSize.y, BaseSize.z * SizeZ);
+        Gizmos.DrawCube(transform.position, aSize);
+    }
+}
+
