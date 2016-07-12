26e8931e359c94c8218e708e0fc03581ffb4c306
Brian Fetter <brian.fetter.dev@gmail.com>
2016-07-12 15:40:37 -0400

    Added ability for mods to call game sound effects.

--- KMFramework/src/KMAudio.cs
+++ KMFramework/src/KMAudio.cs
@@ -5,11 +5,22 @@ public class KMAudio : MonoBehaviour
     public delegate void PlaySoundAtTransformHandler(string name, Transform transform);
     public PlaySoundAtTransformHandler HandlePlaySoundAtTransform;
 
+    public delegate void PlayGameSoundAtTransformHandler(KMSoundOverride.SoundEffect sound, Transform transform);
+    public PlayGameSoundAtTransformHandler HandlePlayGameSoundAtTransform;
+
     public void PlaySoundAtTransform(string name, Transform transform)
     {
         if (HandlePlaySoundAtTransform != null)
         {
             HandlePlaySoundAtTransform(name, transform);
+        }
+    }
+
+    public void PlayGameSoundAtTransform(KMSoundOverride.SoundEffect sound, Transform transform)
+    {
+        if (HandlePlayGameSoundAtTransform != null)
+        {
+            HandlePlayGameSoundAtTransform(sound, transform);
         }
     }
 }
