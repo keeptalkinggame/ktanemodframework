49c28675fa75243e410da7f4b4db31199cb59357
Ben Kane <ben@keeptalkinggame.com>
2016-07-21 11:41:39 -0400

    Remove TriggerDistraction game command for now until custom pacing events are implemented.

--- KMFramework/src/KMGameCommands.cs
+++ KMFramework/src/KMGameCommands.cs
@@ -1,5 +1,8 @@
 ﻿using UnityEngine;
 
+/// <summary>
+/// Interface to directly execute actions related to the game or controlling game flow.
+/// </summary>
 public class KMGameCommands : MonoBehaviour
 {
     public delegate void KMStartMissionCommand(string id, string seed);
@@ -8,9 +11,11 @@ public class KMGameCommands : MonoBehaviour
     public delegate void KMCauseStrikeDelegate(string reason);
     public KMCauseStrikeDelegate OnCauseStrike;
 
-    public delegate void KMTriggerDistractionDelegate(string distractionId, string options);
-    public KMTriggerDistractionDelegate OnTriggerDistraction;
-
+    /// <summary>
+    /// Launches a mission using the given mission id as well as the random seed provided. Useful for triggering deterministic rounds (e.g. for a tournament).
+    /// </summary>
+    /// <param name="id"></param>
+    /// <param name="seed"></param>
     public void StartMission(string id, string seed)
     {
         if (OnStartMission != null)
@@ -19,19 +24,15 @@ public class KMGameCommands : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Cause a strike on the bomb for whatever reason you deem is appropriate.
+    /// </summary>
+    /// <param name="reason"></param>
     public void CauseStrike(string reason)
     {
         if (OnCauseStrike != null)
         {
             OnCauseStrike(reason);
-        }
-    }
-
-    public void TriggerDistraction(string distractionId, string options)
-    {
-        if (OnTriggerDistraction != null)
-        {
-            OnTriggerDistraction(distractionId, options);
         }
     }
 }
