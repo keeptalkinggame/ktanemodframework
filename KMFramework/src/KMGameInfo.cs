02bc7b42da81f77b5282f4ac5dbd03a9291af180
Brian Fetter <brian.fetter.dev@gmail.com>
2016-09-08 16:35:46 -0400

    Adding in alarm clock and lights delegates to game info and implementing unimplemented methods in kmselectable.

--- KMFramework/src/KMGameInfo.cs
+++ KMFramework/src/KMGameInfo.cs
@@ -7,6 +7,33 @@ public class KMGameInfo : MonoBehaviour
 {
     public enum State { Gameplay, Setup, PostGame, Transitioning, Unlock, Quitting }
 
+    /// <summary>
+    /// Delegate type for state changes
+    /// </summary>
     public delegate void KMStateChangeDelegate(State state);
+
+    /// <summary>
+    /// Delegate type for alarm clock state change
+    /// </summary>
+    public delegate void KMAlarmClockChangeDelegate(bool on);
+
+    /// <summary>
+    /// Delegate type for change in lights in the gameplay room
+    /// </summary>
+    public delegate void KMGameplayLightsChangeDelegate(bool on);
+
+    /// <summary>
+    /// Called when game state changes between gameplay, setup, postgame and loading
+    /// </summary>
     public KMStateChangeDelegate OnStateChange;
+
+    /// <summary>
+    /// Called when game state changes between gameplay, setup, postgame and loading
+    /// </summary>
+    public KMAlarmClockChangeDelegate OnAlarmClockChange;
+
+    /// <summary>
+    /// Called when game state changes between gameplay, setup, postgame and loading
+    /// </summary>
+    public KMGameplayLightsChangeDelegate OnGameplayLightsChange;
 }
--- KMFramework/src/KMSelectable.cs
+++ KMFramework/src/KMSelectable.cs
@@ -7,20 +7,101 @@ using System;
 /// </summary>
 public class KMSelectable : MonoBehaviour
 {
+    /// <summary>
+    /// Delegate type for interactions, return is whether it should drill in to children
+    /// </summary>
     public delegate bool OnInteractHandler();
 
+    /// <summary>
+    /// Delegate type for cancelling, return is whether it should drill out to parent
+    /// </summary>
+    public delegate bool OnCancelHandler();
+
+    /// <summary>
+    /// The parent of this selectable
+    /// </summary>
     public KMSelectable Parent;
+
+    /// <summary>
+    /// This is the list of child selectables. 
+    /// Order is important as it is treated as a grid with row length defined by ChildRowLength.
+    /// </summary>
     public KMSelectable[] Children;
+
+    /// <summary>
+    /// Determines if this selectable is essentially a container, currently used for bomb faces
+    /// </summary>
     public bool IsPassThrough;
+
+    /// <summary>
+    /// This is the number of selectables per row for gamepad controls
+    /// </summary>
     public int ChildRowLength;
+
+    /// <summary>
+    /// Sets the object with a highlight mesh for this selectable
+    /// </summary>
     public KMHighlightable Highlight;
-    public Collider SelectableArea;
+
+    /// <summary>
+    /// If you want to set the interaction colliders for mouse to something other than the highlight
+    /// you can set them here.
+    /// </summary>
+    public Collider[] SelectableColliders;
+
+    /// <summary>
+    /// Allows you to set a particular child as the default index for gamepad controls
+    /// </summary>
     public int DefaultSelectableIndex = 0;
 
+    /// <summary>
+    /// Called whenever this selectable becomes the current selection
+    /// </summary>
     public Action OnSelect;
-    public Action OnCancel;
+
+    /// <summary>
+    /// Called whenever this selectable stops being the current selectable
+    /// </summary>
+    public Action OnDeselect;
+
+    /// <summary>
+    /// Called when player backs out of this selectable, an example would be zooming out of a module
+    /// </summary>
+    public OnCancelHandler OnCancel;
+
+    /// <summary>
+    /// Called when player interacts with this selectable. Done on button down
+    /// Returns whether or not it should try to drill to children
+    /// </summary>
     public OnInteractHandler OnInteract;
+
+    /// <summary>
+    /// Called when a player is interacting with this selectable and releases the mouse or controller button
+    /// </summary>
+    public Action OnInteractEnded;
+
+    /// <summary>
+    /// Called when the highlight is turned on, OnSelect is probably more appropriate for most things
+    /// </summary>
     public Action OnHighlight;
+
+    /// <summary>
+    /// Called when player pulls selection stick left while this selectable is focused
+    /// </summary>
     public Action OnLeft;
+
+    /// <summary>
+    /// Called when player pulls selection stick right while this selectable is focused
+    /// </summary>
     public Action OnRight;
+
+    /// <summary>
+    /// Determines whether gamepad selection should wrap around left/right
+    /// </summary>
+    public bool AllowSelectionWrapX = false;
+
+    /// <summary>
+    /// Determines whether gamepad selection should wrap around up/down
+    /// </summary>
+    public bool AllowSelectionWrapY = false;
 }
