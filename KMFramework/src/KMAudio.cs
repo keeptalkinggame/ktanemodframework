380daab42b2cd9382db63ce5e6da79e00677d5ae
Brian Fetter <brian.fetter.dev@gmail.com>
2016-09-08 18:30:10 -0400

    Added support for looping and killing game sound effects.

--- KMFramework/src/KMAudio.cs
+++ KMFramework/src/KMAudio.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 /// <summary>
@@ -6,11 +7,22 @@ using UnityEngine;
 /// </summary>
 public class KMAudio : MonoBehaviour
 {
+    public class KMAudioRef
+    {
+        public Action StopSound;
+    }
+
     public delegate void PlaySoundAtTransformHandler(string name, Transform transform);
     public PlaySoundAtTransformHandler HandlePlaySoundAtTransform;
 
     public delegate void PlayGameSoundAtTransformHandler(KMSoundOverride.SoundEffect sound, Transform transform);
     public PlayGameSoundAtTransformHandler HandlePlayGameSoundAtTransform;
+
+    public delegate KMAudioRef PlaySoundAtTransformWithRefHandler(string name, Transform transform, bool loop = true);
+    public PlaySoundAtTransformWithRefHandler HandlePlaySoundAtTransformWithRef;
+
+    public delegate KMAudioRef PlayGameSoundAtTransformWithRefHandler(KMSoundOverride.SoundEffect sound, Transform transform);
+    public PlayGameSoundAtTransformWithRefHandler HandlePlayGameSoundAtTransformWithRef;
 
     /// <summary>
     /// Play a sound from this mod at the given transform.
@@ -36,5 +48,36 @@ public class KMAudio : MonoBehaviour
         {
             HandlePlayGameSoundAtTransform(sound, transform);
         }
+    }
+
+    /// <summary>
+    /// Play a sound from this mod at the given transform.
+    /// </summary>
+    /// <param name="name"></param>
+    /// <param name="transform"></param>
+    /// <returns>A KMAudioRef that can be used to stop sounds early</returns>
+    public KMAudioRef PlaySoundAtTransformWithRef(string name, Transform transform)
+    {
+        if(HandlePlaySoundAtTransformWithRef != null)
+        {
+            return HandlePlaySoundAtTransformWithRef(name, transform);
+        }
+        return null;
+    }
+
+    /// <summary>
+    /// Play an existing game sound (or its override) at the given transform.
+    /// </summary>
+    /// <param name="sound"></param>
+    /// <param name="transform"></param>
+    /// <returns>A KMAudioRef that can be used to stop sounds early</returns>
+    public KMAudioRef PlayGameSoundAtTransformWithRef(KMSoundOverride.SoundEffect sound, Transform transform)
+    {
+        if(HandlePlayGameSoundAtTransformWithRef != null)
+        {
+            return HandlePlayGameSoundAtTransformWithRef(sound, transform);
+        }
+
+        return null;
     }
 }
