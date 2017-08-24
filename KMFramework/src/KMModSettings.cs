a219edce67c3d9e9cd9b24f968d0aea1d48541c3
Brian Fetter <brian.fetter.dev@gmail.com>
2017-08-24 16:40:27 -0400

    Added ability to refresh ModSettings.

--- KMFramework/src/KMModSettings.cs
+++ KMFramework/src/KMModSettings.cs
@@ -11,4 +11,37 @@ public class KMModSettings : MonoBehaviour
     /// The contents of modSettings.json in this mod's directory, containing any user-specific data they may have put there.
     /// </summary>
     public string Settings;
+
+    /// <summary>
+    /// The location of the ModSettings file that can be used to refresh Settings without reloading the mod
+    /// Set automatically by game overwriting anything that you put here
+    /// </summary>
+    public string SettingsPath;
+
+    /// <summary>
+    /// Refreshes settings from file, requires file access so good to use sparingly
+    /// </summary>
+    public void RefreshSettings()
+    {
+        if (OnRefreshSettings != null)
+        {
+            OnRefreshSettings();
+        }
+    }
+
+    #region Delegates
+    /// <summary>
+    /// Delegate type for refreshing settings
+    /// </summary>
+    public delegate void KMOnRefreshSettingsDelegate();
+
+    #endregion
+
+    #region DO NOT USE IN MOD
+    /// <summary>
+    /// DO NOT USE IN MOD. Used by the game to refresh settings
+    /// </summary>
+    public KMOnRefreshSettingsDelegate OnRefreshSettings;
+
+    #endregion
 }
